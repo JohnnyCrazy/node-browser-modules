c85bdd5a56f18a7c3b3d74b313ffcb7906bceb95
Jonas Dellinger <johnny@johnnycrazy.de>
2016-09-22 23:30:49 +0200

    Fixed paths for windows

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
+function resolve(module) {
+  return require.resolve(module).split('\\').join('/');
+}
+
 exports.emptyShims = [
   'child_process', 'cluster', 'dgram', 'dns', 'fs', 'net', 'readline', 'repl', 'tls', 'tty'
 ];
 
 exports.fileShims = {
-  assert:               require.resolve('assert/'),
-  buffer:               require.resolve('buffer/'),
-  crypto:               require.resolve('crypto-browserify'),
-  domain:               require.resolve('domain-browser'),
-  events:               require.resolve('events/'),
-  http:                 require.resolve('stream-http'),
-  https:                require.resolve('https-browserify'),
-  os:                   require.resolve('os-browserify/browser'),
-  path:                 require.resolve('path-browserify'),
-  process:              require.resolve('process/browser'),
-  punycode:             require.resolve('punycode/'),
-  querystring:          require.resolve('querystring-es3/'),
-  stream:               require.resolve('stream-browserify'),
-  string_decoder:       require.resolve('string_decoder/'),
-  sys:                  require.resolve('util/'),
-  timers:               require.resolve('timers-browserify'),
-  tty:                  require.resolve('tty-browserify'),
-  url:                  require.resolve('url/'),
-  util:                 require.resolve('util/'),
-  vm:                   require.resolve('vm-browserify'),
+  assert:               resolve('assert/'),
+  buffer:               resolve('buffer/'),
+  crypto:               resolve('crypto-browserify'),
+  domain:               resolve('domain-browser'),
+  events:               resolve('events/'),
+  http:                 resolve('stream-http'),
+  https:                resolve('https-browserify'),
+  os:                   resolve('os-browserify/browser'),
+  path:                 resolve('path-browserify'),
+  process:              resolve('process/browser'),
+  punycode:             resolve('punycode/'),
+  querystring:          resolve('querystring-es3/'),
+  stream:               resolve('stream-browserify'),
+  string_decoder:       resolve('string_decoder/'),
+  sys:                  resolve('util/'),
+  timers:               resolve('timers-browserify'),
+  tty:                  resolve('tty-browserify'),
+  url:                  resolve('url/'),
+  util:                 resolve('util/'),
+  vm:                   resolve('vm-browserify'),
 
-  _stream_duplex:       require.resolve('readable-stream/duplex'),
-  _stream_passthrough:  require.resolve('readable-stream/passthrough'),
-  _stream_readable:     require.resolve('readable-stream/readable'),
-  _stream_writable:     require.resolve('readable-stream/writable'),
-  _stream_transform:    require.resolve('readable-stream/transform')
+  _stream_duplex:       resolve('readable-stream/duplex'),
+  _stream_passthrough:  resolve('readable-stream/passthrough'),
+  _stream_readable:     resolve('readable-stream/readable'),
+  _stream_writable:     resolve('readable-stream/writable'),
+  _stream_transform:    resolve('readable-stream/transform')
 };
